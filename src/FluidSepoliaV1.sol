e89bfd1cdb89de014234faca892bfee009f13f1f
Y0sefTamer <programmeryosef@gmail.com>
2026-03-04 14:51:30 +0200

    add openzeppelin proxy and important import

--- src/FluidSepoliaV1.sol
+++ src/FluidSepoliaV1.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
 import {FluidSepoliaLayout} from "./FluidSepoliaLayout.sol";
 
-contract FluidSepoliaV1 {}
+contract FluidSepoliaV1 {
+    
+}
